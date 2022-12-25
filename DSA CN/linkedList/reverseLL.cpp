e2f270c6271f99af0ddf60d990dff04dadaa94e5
Prince Singh <princesingh282002@gmail.com>
2022-12-25 18:39:51 +0530

    reverseLL(recursive O(n))

--- DSA CN/linkedList/reverseLL.cpp
+++ DSA CN/linkedList/reverseLL.cpp
@@ -1,29 +1,63 @@
 #include <iostream>
 #include "main.cpp"
+// worst one approch
+// time complexity O(n2)
+// Node *reverseLL(Node *head)
+// {
+//     if (head == NULL || head->next == NULL)
+//     {
+//         return head;
+//     }
 
-Node *reverseLL(Node *head)
+//     Node *smallOutput = reverseLL(head->next);
+//     Node *temp = smallOutput;
+//     while (temp->next != NULL)
+//     {
+//         temp = temp->next;
+//     }
+//     temp->next = head;
+//     head->next = NULL;
+//     head = smallOutput;
+//     return head;
+// }
+///////////////////////////////////////////////////////
+// better approach
+//  tn o(n)
+
+class Pair
+{
+public:
+    Node *head;
+    Node *tail;
+};
+
+Pair reverseLL(Node *head)
 {
     if (head == NULL || head->next == NULL)
     {
-        return head;
+        Pair ans;
+        ans.head = head;
+        ans.tail = head;
+        return ans;
     }
-
-    Node *smallOutput = reverseLL(head->next);
-    Node *temp = smallOutput;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = head;
+    Pair smallOut = reverseLL(head->next);
+    smallOut.tail->next = head;
     head->next = NULL;
-    head = smallOutput;
-    return head;
+    Pair ans;
+    ans.head = smallOut.head;
+    ans.tail = head;
+    return ans;
 }
 
+Node *reverseLLRun(Node *head)
+{
+    Pair ans = reverseLL(head);
+    return ans.head;
+}
 int main()
 {
     Node *head = takeInput_Better();
     // print(head);
-    Node *temp = reverseLL(head);
+    Node *temp = reverseLLRun(head);
     print(temp);
 }
