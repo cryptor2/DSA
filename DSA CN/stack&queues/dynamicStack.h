6e79722f282a0f002057e8a997f71b6327eb86e5
Prince Singh <princesingh282002@gmail.com>
2023-01-06 22:09:02 +0530

    dynamicStackUsingArray

---
+++ DSA CN/stack&queues/dynamicStack.h
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+class StackUsingArray
+{
+    int *data;
+    int capacity;
+    int nextIndex = 0;
+
+public:
+    StackUsingArray()
+    {
+        data = new int[capacity];
+        nextIndex = 0;
+    }
+    int size()
+    {
+        return nextIndex;
+    }
+
+    bool isEmpty()
+    {
+        return nextIndex == 0;
+    }
+
+    void push(int number)
+    {
+        if (nextIndex == capacity)
+        {
+            int *newData = new int[2 * capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                newData[i] = data[i];
+            }
+            capacity = 2 * capacity;
+            delete[] data;
+            data = newData;
+        }
+        data[nextIndex] = number;
+        nextIndex++;
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty";
+            return INT_MIN;
+        }
+        else
+        {
+            nextIndex--;
+            return data[nextIndex];
+        }
+    }
+
+    int top()
+    {
+        return data[nextIndex - 1];
+    }
+};
---
+++ DSA CN/stack&queues/useStack.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "dynamicStack.h"
+using namespace std;
+int main()
+{
+    StackUsingArray v;
+    v.push(10);
+    cout << v.top();
+}
