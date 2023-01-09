00f1be090976cd7b889d92a504b5307f16dc2f04
Prince Singh <princesingh282002@gmail.com>
2023-01-09 22:08:42 +0530

    trees

---
+++ DSA CN/trees/treeUse.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include "tree.h"
+using namespace std;
+
+TreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "enter root data" << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cout << "Enter the number of children of " << front->data << endl;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++)
+        {
+            int childData;
+            cout << "Enter the " << i << " th child of " << front->data << endl;
+            cin >> childData;
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
+int main()
+{
+    TreeNode<int> *root = takeInputLevelWise();
+    printTree(root);
+}
