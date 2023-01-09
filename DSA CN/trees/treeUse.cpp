b0739437a2853a3fc8919d692130f5ea23323457
Prince Singh <princesingh282002@gmail.com>
2023-01-09 22:46:38 +0530

    trees

--- DSA CN/trees/treeUse.cpp
+++ DSA CN/trees/treeUse.cpp
@@ -31,6 +31,24 @@ TreeNode<int> *takeInputLevelWise()
     return root;
 }
 
+void printTreeLevelWise(TreeNode<int> *root)
+{
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (!pendingNodes.empty())
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        for (int i = 0; i < front->children.size(); i++)
+        {
+            cout << front->children[i]->data;
+            pendingNodes.push(front->children[i]);
+        }
+    }
+}
+
 void printTree(TreeNode<int> *root)
 {
     if (root == NULL)
