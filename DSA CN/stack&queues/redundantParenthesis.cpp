513ea5bbb0b43726dd498fd49bc16baeb4540c9b
Prince Singh <princesingh282002@gmail.com>
2023-01-10 20:47:56 +0530

    check unwanted parenthesis

---
+++ DSA CN/stack&queues/redundantParenthesis.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool redundantParenthesis(string s)
+{
+    int count = 0;
+    stack<char> stk;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            stk.push(s[i]);
+        }
+        else if (s[i] != ')' && stk.size() != 0)
+        {
+            count++;
+        }
+        else if (s[i] == ')')
+        {
+            if (count <= 1)
+            {
+                return true;
+            }
+            else if (count > 1)
+            {
+                stk.pop();
+                count = 0;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    cout << redundantParenthesis(s);
+}
