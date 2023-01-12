8f01b9179dd9553c3df213acb8346ee5fbd5afb6
Prince Singh <princesingh282002@gmail.com>
2023-01-12 23:07:42 +0530

    binaryTree

--- DSA CN/binary tree/bstUSE.cpp
+++ DSA CN/binary tree/bstUSE.cpp
@@ -40,6 +40,78 @@ BinaryTreeNode<int> *takeInputBetter()
     return root;
 }
 
+void mirror(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    mirror(root->left);
+    mirror(root->right);
+    BinaryTreeNode<int> *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+}
+
+int heightOfTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return 0;
+    return 1 + max(heightOfTree(root->left), heightOfTree(root->right));
+}
+bool findNodeX(BinaryTreeNode<int> *root, int x)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    if (root->data == x)
+    {
+        return true;
+    }
+    bool ansL = findNodeX(root->left, x);
+    bool ansR = findNodeX(root->right, x);
+    return ansL + ansR;
+}
+
+int countNode(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return 0;
+    return 1 + countNode(root->left) + countNode(root->right);
+}
+void printLevelWise(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        if (front->left != NULL)
+        {
+            cout << "L:" << front->left->data << ",";
+            pendingNodes.push(front->left);
+        }
+        else
+            cout << "L:" << -1 << ",";
+
+        if (front->right != NULL)
+        {
+            cout << "R:" << front->right->data;
+            pendingNodes.push(front->right);
+        }
+        else
+            cout << "R:" << -1;
+        cout << endl;
+    }
+}
+
 void inorder(BinaryTreeNode<int> *root)
 {
     if (root == NULL)
@@ -54,6 +126,15 @@ void inorder(BinaryTreeNode<int> *root)
 int main()
 {
     BinaryTreeNode<int> *root = takeInputBetter();
-    inorder(root);
+    // inorder(root);
+    // printLevelWise(root);
+    // cout << "No of nodes are: " << countNode(root);
+    // int x;
+    // cout << "Enter the value of X" << endl;
+    // cin >> x;
+    // cout << "x is present " << findNodeX(root, x);
+    // cout << heightOfTree(root);
+    mirror(root);
+    printLevelWise(root);
     delete root;
 }
