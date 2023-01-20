0b903555352e26ad06ba92977b3b26d0558b989c
Prince Singh <princesingh282002@gmail.com>
2023-01-20 15:02:54 +0530

    bubbleSort

--- DSA CN/linkedList/assignmentLL2/bubbleSort.cpp
+++ DSA CN/linkedList/assignmentLL2/bubbleSort.cpp
@@ -12,6 +12,7 @@ public:
         this->next = NULL;
     }
 };
+
 Node *takeInput_Better()
 {
     int data;
@@ -35,6 +36,7 @@ Node *takeInput_Better()
     }
     return head;
 }
+
 void print(Node *head)
 {
     Node *temp = head;
@@ -46,6 +48,78 @@ void print(Node *head)
     cout << endl;
 }
 
-Node *BubbleSort(Node *head)
+int len(Node *head)
 {
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    // cout << "len " << len << endl;
+    return count;
+}
+
+Node *bubbleSort(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    // cout << "length call " << endl;
+    int length = len(head);
+    cout << "length  " << length;
+    int swap;
+    cout << "BubbleSort";
+    for (int i = 0; i <= length; i++)
+    {
+        Node *prev = NULL;
+        Node *c = head;
+        Node *next = c->next;
+        swap = 0;
+        while (c->next != NULL)
+        {
+            if (c->data <= next->data)
+            {
+                prev = c;
+                c = next;
+                next = next->next;
+            }
+            else if (c->data > next->data)
+            {
+                if (prev == NULL)
+                {
+                    head = c->next;
+                    c->next = head->next;
+                    head->next = c;
+                    prev = head;
+                    next = c->next;
+                }
+                else
+                {
+                    c->next = next->next;
+                    prev->next = next;
+                    next->next = c;
+                    prev = prev->next;
+                    next = c->next;
+                }
+                swap = 1;
+            }
+        }
+        if (swap == 0)
+        {
+            return head;
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    Node *head = takeInput_Better();
+    print(head);
+    // cout << "BubbleSort call" << len(head);
+    head = bubbleSort(head);
+    print(head);
 }
